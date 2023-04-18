33fc6e13a1581eb720152c879e65de497e7312e4
BoTLeFt <erik_shahov@mail.ru>
2023-04-18 21:27:30 +0300

    Commit for SwimmingPool task

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,20 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Input your value:");
-        int value = new Scanner(System.in).nextInt();
-        for (int i = 1; i <= value; i = i + 1) {
-            if (value % i == 0) {
-                System.out.println(i+"*"+value/i);
-            }
+        int volume = 1200;
+        int fillingSpeed = 30; //30 litres per minute
+
+        int devastationSpeed = 10; //10 litres per minute
+
+        int fillingSpeedAfterDevastation = fillingSpeed - devastationSpeed;
+        if (fillingSpeedAfterDevastation<=0) {
+            System.out.println("Impossible to fill pool");
         }
+        int pool = 0;
+        int itterationsCount = 0;
+        while (pool<volume){
+            pool = pool + fillingSpeedAfterDevastation;
+            itterationsCount = itterationsCount + 1;
+        }
+        System.out.println(itterationsCount+" minutes");
     }
 }
