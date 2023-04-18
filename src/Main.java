e3a43508710e760e71cdbfa64baddf54276c9f22
BoTLeFt <erik_shahov@mail.ru>
2023-04-18 21:37:14 +0300

    Commit for GuessTheNumber task

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int volume = 1200;
-        int fillingSpeed = 30; //30 litres per minute
-
-        int devastationSpeed = 10; //10 litres per minute
-
-        int fillingSpeedAfterDevastation = fillingSpeed - devastationSpeed;
-        if (fillingSpeedAfterDevastation<=0) {
-            System.out.println("Impossible to fill pool");
+        int value = new Random().nextInt(100);
+        while (true) {
+            int attempt = new Scanner(System.in).nextInt();
+            if (attempt==value) {
+                System.out.println("You are right! Guessed number is "+value);
+                break;
+            } else if (attempt < value) {
+                System.out.println("Try to guess a bit higher");
+            } else {
+                System.out.println("Try to guess a bit lower");
+            }
         }
-        int pool = 0;
-        int itterationsCount = 0;
-        while (pool<volume){
-            pool = pool + fillingSpeedAfterDevastation;
-            itterationsCount = itterationsCount + 1;
-        }
-        System.out.println(itterationsCount+" minutes");
     }
 }
